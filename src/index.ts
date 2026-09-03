981e528314ac20c7ff51afe29753b4c3a8a3d473
Anis Aladin Rangrez <anisaladinrangrez@gmail.com>
2026-09-04 00:16:10 +0530

    fix(runtime): instantiate Sound HybridObject via NitroModules.createHybridObject and compile lib

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import { NitroModules } from 'react-native-nitro-modules';
 import type { Sound as SoundSpec } from './specs/Sound.nitro';
-export type { AudioCategory, NotificationOptions, NotificationPriority, Sound as SoundType, SoundOptions, } from './specs/Sound.nitro';
+
+export type {
+  AudioCategory,
+  NotificationOptions,
+  NotificationPriority,
+  Sound as SoundType,
+  SoundOptions,
+} from './specs/Sound.nitro';
 export type { SoundPlayer } from './specs/SoundPlayer.nitro';
-export declare const Sound: SoundSpec;
+
+export const Sound = NitroModules.createHybridObject<SoundSpec>('Sound');
